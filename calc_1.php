8f81d5891a1e27b932eb9ab9ca77692516802f4a
FaridunFaizulloev <faridun.faizulloev@mail.ru>
2020-06-20 03:02:54 +0300

    lab-7

---
+++ calc_1.php
@@ -0,0 +1,31 @@
+﻿<?php
+if (!empty($_POST)) {
+ $x = !empty($_POST['x']) ? $_POST['x'] : '';
+ $y = !empty($_POST['y']) ? $_POST['y'] : '';
+ $operation = !empty($_POST['operation']) ?$_POST['operation']: '';
+ if (is_int($x) || is_int($y)) {
+ echo 'Неверный тип данных';
+ } else {
+ switch ($operation) {
+ case 'sum':
+ $result = $x.'+'.$y.'=' . ($x + $y);
+ break;
+ case 'diff':
+ $result = $x.'-'.$y.'=' . ($x - $y);
+ break;
+ case 'del':
+ if($x ==0 or $y== 0) {echo "Передано значение 0";
+ break;}
+{$result = $x.'÷'.$y.'=' . ($x / $y);}
+ break;
+ case 'um':
+ if($x ==0 or $y== 0) {echo "Передано значение 0";
+ break;}
+ $result = $x.'×'.$y.'=' . ($x * $y);
+ break;
+ default:
+ $result = 'Передан неизвестный тип операции';
+ }
+ echo $result;
+ }
+}
---
+++ index_1.php
@@ -0,0 +1,17 @@
+﻿<?php
+echo "<br>","Введите переменные:";
+echo "<br>";
+echo "Выражение:   ";
+include 'calc.php';
+?>
+<form action="/index.php" method="POST">
+ <input type="text" name="x">
+ <select name="operation">
+ <option value="sum">+</option>
+ <option value="diff">-</option>
+ <option value="del">÷</option>
+ <option value="um">×</option>
+ </select>
+ <input type="text" name="y">
+ <button type="submit">Выполнить действие</button>
+</form>
